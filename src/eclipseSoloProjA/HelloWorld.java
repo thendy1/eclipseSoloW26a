48932eab7f4c7c71fc2ef1c6a0db35d6bb65048a
thendy1 <thendy@emich.edu>
2026-02-16 13:48:52 -0500

    programmer 2 first change

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github project");
 		System.out.println("Add another line!");
+		System.out.println("Programmer 2 did this");
 	}
 
 }
